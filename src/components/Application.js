fb630e1f270685f4ef4eaea917ba9f1f55160a64
Yirong Qin <yq4103@mun.ca>
2021-09-30 23:07:58 -0400

    combine the state for day, days, and appointments into a state into a single object

--- src/components/Application.js
+++ src/components/Application.js
@@ -8,52 +8,65 @@ import DayList from "./DayList";
 
 import Appointment from "components/Appointment";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id:4,
-    time: "3pm",
-    interview: {
-      student: "Zoey Qin",
-      interviewer: {
-        id: 6,
-        name: "林巾帼",
-        avatar: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/760.png",
-      }
-    }
-  }
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm",
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png",
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm",
+//   },
+//   {
+//     id:4,
+//     time: "3pm",
+//     interview: {
+//       student: "Zoey Qin",
+//       interviewer: {
+//         id: 6,
+//         name: "林巾帼",
+//         avatar: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/760.png",
+//       }
+//     }
+//   }
+// ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
   const parsedAppointment = appointments.map(appointment  => 
     <Appointment  key={appointment.id} {...appointment}/>);
-  const [days, setDays] = useState([]);
+  //combine the state for day, days, and appointments into a state into a single object
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
 
+  const dailyAppointments = [];
+  
+  //function which updates the state with the new day
+  const setDay = day => setState({ ...state, day });
+  const setDays = (days) => {
+    setState(prev => ({ ...prev, days }));
+  }
+
+  // Hardcoded days array removed so as to fetch axios data instead
   useEffect(() => {
     axios.get("/api/days").then(response=> {
-      setDays([response.data])
+      setDays(response.data)
     })
-  }, [])
+  }, []);
   return (
     <main className="layout">
       <section className="sidebar">
@@ -64,7 +77,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList day={state.day} days={state.days} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+export function getAppointmentsForDay(state, day) {
+  //empty resut array
+  const result = [];
+  //the find method returns the value of the first element in the array that satisfies the condition
+  //to find within the days array the object with a name key whose value equals the day specified
+  const filteredDay = state.days.find(apptDay => apptDay.name === day)
+  //If there are no appointments on the given day, our days data will be empty, return an empty array
+  if (!filteredDay) {
+    return result;
+  }
+  //the value of the appointment key of the days object is an array of appointments ids, iterate through the array and push the corresponding appointments object to the result array 
+  for (const apptId of filteredDay.appointments) {
+    result.push(state.appointments[apptId]);
+  }
+
+  return result;
+}
