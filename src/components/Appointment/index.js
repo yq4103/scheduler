37efb12d21b4d3026a4572a9e8c876c9f7d76703
Yirong Qin <yq4103@mun.ca>
2021-10-04 23:23:17 -0400

    error handling

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -7,39 +7,57 @@ import Empty from "./Empty";
 import useVisualMode from "hooks/useVisualMode";
 import Form from "components/Appointment/Form"
 import Status from "./Status";
+import Confirm from "./Confirm";
+import Error from "./Error";
 
 export default function Appointment(props) {
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
   const CREATE = "CREATE";
   const SAVING = "SAVING";
-  const DELETING = "DELETING"
+  const DELETING = "DELETING";
+  const CONFIRM = "CONFIRM";
+  const EDIT = "EDIT";
+  const ERROR_SAVE = "ERROR_SAVE";
+  const ERROR_DELETE = "ERROR_DELETE";
 
+  //save an interview to the database and to the app
   function save(name, interviewer) {
     const interview = {
       student: name,
       interviewer
     };
 
-    transition(SAVING)
-
-    props.bookInterview(props.id, interview)
-    .then(()=>{
-      transition(SHOW)
-    })
-    
+    transition(SAVING);
+    props
+    .bookInterview(props.id, interview)
+    .then(() => transition(SHOW))
+    //error handling when axios rejects the Promise
+    .catch(error => transition(ERROR_SAVE));
   }
 
-  function deleteInterview(id) {
+  //add a confirmation message when a user tries to delete an interview appointment
+  const deleteConfirm = () => {
+    transition(CONFIRM)
+  };
 
-    transition(DELETING)
+  //after a user confirms, delete an interview from the database and from the app
+  function deleteInterview(event) {
+
+    transition(DELETING, true)
 
     props.cancelInterview(props.id)
-    .then(()=>{
-      transition(EMPTY)
-    })
+    .then(()=>{transition(EMPTY)})
+    //error handling when axios rejects the Promise
+    .catch(error => transition(ERROR_DELETE, true));
   }
 
+  //edit an interview
+  const editAppointment = () => {
+    transition(EDIT);
+  };
+
+  //a custom hook that allows our Appointment components to keep track of the "mode" that they are in, as a user interacts with them to do things
   const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
@@ -51,12 +69,23 @@ export default function Appointment(props) {
       <Show
         student={props.interview.student}
         interviewer={props.interview.interviewer}
-        onDelete={deleteInterview}
+        onDelete={deleteConfirm}
+        onEdit={editAppointment}
       />
     )}
     {mode === CREATE && <Form interviewers={props.interviewers} onCancel={back} onSave={save}/>}
     {mode === SAVING && <Status message="Saving" />}
     {mode === DELETING && <Status message="Deleting" />}
+    {mode === CONFIRM && <Confirm message="Are you sure you would like to delete?" onConfirm={deleteInterview} onCancel = {() => back()} />}
+    {mode === EDIT &&  <Form 
+      name={props.interview.student}
+      interviewer={props.interview.interviewer.id}
+      interviewers={props.interviewers}
+      onCancel = {() => back()}
+      onSave = {save}
+    />}
+    {mode === ERROR_SAVE && (<Error message="Could not save appointment" onClose={() => back()} />)}
+    {mode === ERROR_DELETE && (<Error message="Could not cancel appointment" onClose={() => back()} />)}
   </article>
   );
 }
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -1,13 +1,15 @@
 export function getAppointmentsForDay(state, day) {
   //empty result array
   const result = [];
-  const filteredDay = state.days.filter(filDay => filDay.name === day)
-
-  if (!filteredDay[0]) {
+  //the find method returns the value of the first element in the array that satisfies the condition
+  //to find within the days array the object with a name key whose value equals the day specified
+  const filteredDay = state.days.find(apptDay => apptDay.name === day)
+  //If there are no appointments on the given day, our days data will be empty, return an empty array
+  if (!filteredDay) {
     return result;
   }
-
-  for (const apptId of filteredDay[0].appointments) {
+  //the value of the appointment key of the days object is an array of appointments ids, iterate through the array and push the corresponding appointments object to the result array 
+  for (const apptId of filteredDay.appointments) {
     result.push(state.appointments[apptId]);
   }
 
@@ -27,13 +29,15 @@ export function getInterview(state, interview) {
 export function getInterviewersForDay(state, day) {
   //empty result array
   const result = [];
-  const filteredDay = state.days.filter(filDay => filDay.name === day)
-
-  if (!filteredDay[0]) {
+  //the find method returns the value of the first element in the array that satisfies the condition
+  //to find within the days array the object with a name key whose value equals the day specified
+  const filteredDay = state.days.find(apptDay => apptDay.name === day)
+  //If there are no appointments on the given day, our days data will be empty, return an empty array
+  if (!filteredDay) {
     return result;
   }
-
-  for (const apptId of filteredDay[0].interviewers) {
+  //the value of the interviewers key of the days object is an array of interviewers ids, iterate through the array and push the corresponding interviewers object to the result array 
+  for (const apptId of filteredDay.interviewers) {
     result.push(state.interviewers[apptId]);
   }
 
